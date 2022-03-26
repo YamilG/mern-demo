a9d51b4a07d7fa73bb5c9865133bc4ba880f7651
Yamil Gonzales <yamilg@gmail.com>
2022-03-26 17:09:30 -0600

    goals controller and routes created

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,13 @@
-console.log('Hello World');
+const express = require('express');
+const dotenv = require('dotenv').config();
+const { errorHandler } = require('./middleware/errorMiddleware');
+const port = process.env.PORT || 8001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/api/goals', require('./routes/goalRoutes'));
+app.use(errorHandler); // this will automatically override the default error handler
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
